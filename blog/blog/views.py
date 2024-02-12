373e56e8ee13320e3494894863c2c99beb06ee29
obamwonyi <developer.oduwa@gmail.com>
2024-02-12 15:00:25 +0100

    written test for the post model and related routes

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
+
+
+class BlogListView(ListView):
+    """
+    This class would be responsible for
+    adding the generic view for the base.html template
+    for the blog post home route , which would
+    make use of the model, Post as object resource
+    to generate list from .
+    """
+    model = Post
+    template_name = "home.html"
+
+
+class BlogDetailView(DetailView):
+    """
+    This class would be responsible for adding the generic
+    view for loading our various post in details, like
+    dedicating a particular view to one post
+    """
+    model = Post
+    template_name = "post_detail.html"
